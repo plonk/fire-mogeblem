be7f28dcdf42bf085eed95eff7ab21bd8ae1c050
Yoteichi <plonk@piano.email.ne.jp>
2018-06-02 21:07:37 +0900

    メッセージを変えた。

--- test.lisp
+++ test.lisp
@@ -159,7 +159,7 @@ CL-USER 10 > (minimum '((a 1) (b -1) (c -2)) #'< #'second)
 	(error "start-color error ~s." ret-code)))
   (unless (= 256 charms/ll:*COLORS*)
     (when (eql (charms/ll:can-change-color) charms/ll:false)
-      (error "Your terminal does not support color.")))
+      (error "Your terminal cannot change colors.")))
   (charms/ll:use-default-colors)
   (charms/ll:curs-set 0))
 
